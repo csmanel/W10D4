d2edfc848b1ef4dfe102e839a4617f2165b21f50
Levi Sherman <Levi Sherman>
2023-12-21 14:30:58 -0500

    completed phase 2

---
+++ art-museum/src/components/GalleryNavigation/GalleryNavigation.jsx
@@ -0,0 +1,17 @@
+
+import { NavLink } from "react-router-dom"
+
+
+function GalleryNavigation({galleries}) {
+
+    return (
+        <nav>
+            <h1>Galleries</h1>
+            {galleries.map(gallery => {
+                return <NavLink key={gallery.id} to="galleries/{gallery.galleryid}">{gallery.name}</NavLink>
+            })}
+        </nav>
+    )
+}
+
+export default GalleryNavigation
