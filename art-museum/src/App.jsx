2341d61bb83c72224ce4d912f3e7c202aa5d3358
csmanel <csmanel@gmail.com>
2023-12-21 14:58:36 -0500

    finish phase 3

--- art-museum/src/App.jsx
+++ art-museum/src/App.jsx
@@ -1,31 +1,44 @@
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
-import harvardArt from "./data/harvardArt"
-import GalleryNavigation from "./components/GalleryNavigation/GalleryNavigation";
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import harvardArt from './data/harvardArt';
+import GalleryNavigation from './components/GalleryNavigation/GalleryNavigation';
 
-const router = createBrowserRouter([
-  { 
-    path: "*",
-    element: <h2>Page Not Found</h2>
-  },
-  {
-    path: '/',
-    element: <>
-      <h2>Harvard Art Museum</h2> 
-      <p>Look, but Don&apos;t Touch. Please select a Gallery in the navigation bar.</p>
-    </>
-  },
-  {
-    path: 'galleries',
-    element: <GalleryNavigation galleries={harvardArt.records}/>
-  }
-
-]);
-
-
-
-function App() {
-  return <RouterProvider router={router}/>;
+function Layout() {
+  return (
+    <div className="page-wrapper">
+      <GalleryNavigation galleries={harvardArt.records} />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
 }
 
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: (
+          <>
+            <h2>Harvard Art Museum</h2>
+            <p>
+              Look, but Don&apos;t Touch. Please select a Gallery in the
+              navigation bar.
+            </p>
+          </>
+        ),
+      },
+      {
+        path: '*',
+        element: <h2>Page Not Found</h2>,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
 
 export default App;
--- art-museum/src/components/GalleryNavigation/GalleryNavigation.jsx
+++ art-museum/src/components/GalleryNavigation/GalleryNavigation.jsx
@@ -1,17 +1,19 @@
+import './GalleryNavigation.css';
+import { NavLink } from 'react-router-dom';
 
-import { NavLink } from "react-router-dom"
-
-
-function GalleryNavigation({galleries}) {
-
-    return (
-        <nav>
-            <h1>Galleries</h1>
-            {galleries.map(gallery => {
-                return <NavLink key={gallery.id} to="galleries/{gallery.galleryid}">{gallery.name}</NavLink>
-            })}
-        </nav>
-    )
+function GalleryNavigation({ galleries }) {
+  return (
+    <nav>
+      <h1>Galleries</h1>
+      {galleries.map((gallery) => {
+        return (
+          <NavLink key={gallery.id} to={`galleries/${gallery.galleryid}`} end>
+            {gallery.name}
+          </NavLink>
+        );
+      })}
+    </nav>
+  );
 }
 
-export default GalleryNavigation
+export default GalleryNavigation;
